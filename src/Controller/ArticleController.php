12a23478aac534d60c07be5c5a538f2537a9969d
Rémi Dubreucq <Kazzouwi59@gmail.com>
2023-05-08 12:13:01 +0200

    Création des pages avec twig

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -7,9 +7,60 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
-    #[Route('/article', name: 'article')]
-    public function connection()
+    #[Route('/articles', name: 'article_list')]
+    public function articleList()
     {
-        return $this->render('article.html.twig');
+        $articles = [
+            [
+                'id' => 1,
+                'title' => 'Article 1',
+                'content' => 'Lorem ipsum dolor sit amet'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Article 2',
+                'content' => 'Lorem ipsum dolor sit amet'
+            ],
+            [
+                'id' => 3,
+                'title' => 'Article 3',
+                'content' => 'Lorem ipsum dolor sit amet'
+            ],
+        ];
+
+        $isSidebarDisplayed = false;
+
+        return $this->render('articleList.html.twig', [
+            'articles' => $articles,
+            'isSidebarDisplayed' => $isSidebarDisplayed
+        ]);
+    }
+
+    #[Route('/articles/{id}', name: 'article_show')]
+    public function articleShow($id)
+    {
+        $articles = [
+            [
+                'id' => 1,
+                'title' => 'Article 1',
+                'content' => 'Lorem ipsum dolor sit amet'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Article 2',
+                'content' => 'Lorem ipsum dolor sit amet'
+            ],
+            [
+                'id' => 3,
+                'title' => 'Article 3',
+                'content' => 'Lorem ipsum dolor sit amet'
+            ],
+        ];
+
+        $article = $articles[$id - 1];
+
+        return $this->render('articleShow.html.twig', [
+            'article' => $article
+        ]);
     }
 }
