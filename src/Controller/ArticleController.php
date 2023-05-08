5eb60af7c8b10f1f0e291bcb72c7e65eb390fd2f
Rémi Dubreucq <Kazzouwi59@gmail.com>
2023-05-08 16:53:29 +0200

    Mise en place des catégories

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,72 +2,28 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
     #[Route('/articles', name: 'article_list')]
-    public function articleList()
-    {
-        $articles = [
-            [
-                'id' => 1,
-                'title' => 'Article 1',
-                'content' => 'Lorem ipsum dolor sit amet'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Article 2',
-                'content' => 'Lorem ipsum dolor sit amet'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Article 3',
-                'content' => 'Lorem ipsum dolor sit amet'
-            ],
-            [
-                'id' => 4,
-                'title' => 'Article 4',
-                'content' => 'Lorem ipsum dolor sit amet'
-            ],
-        ];
+    public function articleList(ArticleRepository $articleRepository)
+    {       
+        $articles = $articleRepository->findAll();
 
-        $isSidebarDisplayed = false;
 
+      
         return $this->render('articleList.html.twig', [
-            'articles' => $articles,
-            'isSidebarDisplayed' => $isSidebarDisplayed
+            'articles' => $articles
         ]);
     }
 
     #[Route('/articles/{id}', name: 'article_show')]
-    public function articleShow($id)
+    public function articleShow(ArticleRepository $articleRepository, $id)
     {
-        $articles = [
-            [
-                'id' => 1,
-                'title' => 'Article 1',
-                'content' => 'Lorem ipsum dolor sit amet'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Article 2',
-                'content' => 'Lorem ipsum dolor sit amet'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Article 3',
-                'content' => 'Lorem ipsum dolor sit amet'
-            ],
-            [
-                'id' => 4,
-                'title' => 'Article 4',
-                'content' => 'Lorem ipsum dolor sit amet'
-            ],
-        ];
-
-        $article = $articles[$id - 1];
+        $article = $articleRepository->find($id);
 
         return $this->render('articleShow.html.twig', [
             'article' => $article
